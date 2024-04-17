570d9fe2215132be8b8b9aa577ed5166def76953
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-17 00:18:07 -0300

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,11 @@ const apiDevBurguer = axios.create({
   baseURL: "http://localhost:3001",
 });
 
+apiDevBurguer.interceptors.request.use(async (config) => {
+  const userData = await localStorage.getItem("codeburger:userData");
+  const token = userData && JSON.parse(userData).token;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
 export default apiDevBurguer;

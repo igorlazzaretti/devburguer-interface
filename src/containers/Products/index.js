7e087c46ef1b746f76f289c878eee51f5de96a7d
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-29 23:24:57 -0300

    commit

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import ProductsIMG from '../../assets/Products/home-main-img-2dir.png'
-import { Container, ProductsBanner} from './styles'
-
+import {
+  Container,
+  ProductsBanner,
+  CategoryButton,
+  CategoryNav,
+} from './styles'
+import api from '../../services/api'
 
 function Products() {
-  return ( 
-  <Container>
-    <ProductsBanner src={ProductsIMG} alt='Logo da Home'style={{ width: '100%', height:'30vh', objectFit:'cover' }}/>
+  // Chamada API para Nav
+  const [categories, setCategories] = useState([])
+  const [activeCategory, setActiveCategory] = useState([0])
 
-  </Container>
-)}
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('categories')
+      const newCategories = [{ id: 0, name: 'Todas' }, ...data]
+      setCategories(newCategories)
+      //   console.log('consoel do carousel response',response.data)
+    }
+    loadCategories()
+  }, [])
+
+  return (
+    <Container>
+      <ProductsBanner
+        src={ProductsIMG}
+        alt="Logo da Home"
+        style={{ width: '100%', height: '30vh' }}
+      />
+      <CategoryNav>
+        {categories &&
+          categories.map(category => (
+            <CategoryButton 
+              type="button"
+              key={category.id}
+              isActiveCategory={activeCategory === category.id}
+              onClick={() => {setActiveCategory(category.id)}}
+            > 
+              {category.name}
+            </CategoryButton>
+          ))}
+      </CategoryNav>
+    </Container>
+  )
+}
 
 export default Products
--- src/containers/Products/styles.js
+++ src/containers/Products/styles.js
@@ -4,4 +4,24 @@ export const Container = styled.div``
 
 export const ProductsBanner = styled.img`
     width: 100%;
+    object-fit: cover;
+`
+export const CategoryNav = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 50px;
+    margin-top: 20px;
+`
+
+
+export const CategoryButton = styled.button`
+    cursor: pointer;
+    background: none;
+    border: none;
+    color: ${props => props.isActiveCategory ? '#7843e5' : '#999999'};
+    border-bottom: ${props => props.isActiveCategory && '2px solid #7843e5' };
+    font-size: 17px;
+    line-height: 20px;
+    padding-bottom: 5px;
+
 `
